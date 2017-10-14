a5492f665d72656132df18fa61a521e696b9acc7
Kiwon Jeong <kiwonjg@uw.edu>
2017-10-13 21:11:48 -0700

    unfinished v1

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -24,16 +24,68 @@ open class TestMe {
 // Money
 //
 public struct Money {
-  public var amount : Int
-  public var currency : String
+    public var amount : Int
+    public var currency : String
+
+    init(amount: Int, currency: String) {
+        self.amount = amount
+        self.currency = currency
+    }
+
+    public func convert(_ to: String) -> Money {
+        if (currencyCheck(to)) {
+            var newAmount = self.amount
+            let oldCurr = currency
+            switch oldCurr {
+            case "GBP":
+                newAmount *= 2
+            case "EUR":
+                newAmount *= 2
+                newAmount /= 3
+            case "CAN":
+                newAmount *= 4
+                newAmount /= 5
+            default:
+                newAmount *= 1
+            }
+            switch to {
+            case "GBP":
+                newAmount /= 2
+            case "EUR":
+                newAmount /= 2
+                newAmount *= 3
+            case "CAN":
+                newAmount /= 4
+                newAmount *= 5
+            default:
+                newAmount *= 1
+            }
+            return Money(amount: newAmount, currency: to)
+        }
+        return self
+    }
   
-  public func convert(_ to: String) -> Money {
-  }
-  
-  public func add(_ to: Money) -> Money {
-  }
-  public func subtract(_ from: Money) -> Money {
-  }
+    public func add(_ to: Money) -> Money {
+        if (currencyCheck(to.currency)) {
+            let prev = convert(to.currency)
+            return Money(amount: prev.amount + to.amount, currency: to.currency)
+        }
+        return to
+    }
+    public func subtract(_ from: Money) -> Money {
+        if (currencyCheck(from.currency)) {
+            let prev = convert(from.currency)
+            return Money(amount: from.amount - prev.amount, currency: from.currency)
+        }
+        return from
+    }
+    
+    private func currencyCheck(_ to: String) -> Bool {
+        if (to == "GBP" || to == "EUR" || to == "USD" || to == "CAN") {
+            return true;
+        }
+        return false;
+    }
 }
 
 ////////////////////////////////////
@@ -49,9 +101,12 @@ open class Job {
   }
   
   public init(title : String, type : JobType) {
+    self.title = title // here
+    self.type = type // here
   }
   
   open func calculateIncome(_ hours: Int) -> Int {
+    return 0 // here
   }
   
   open func raise(_ amt : Double) {
@@ -68,14 +123,14 @@ open class Person {
 
   fileprivate var _job : Job? = nil
   open var job : Job? {
-    get { }
+    get { return nil } // here
     set(value) {
     }
   }
   
   fileprivate var _spouse : Person? = nil
   open var spouse : Person? {
-    get { }
+    get { return nil } // here
     set(value) {
     }
   }
@@ -87,6 +142,7 @@ open class Person {
   }
   
   open func toString() -> String {
+    return "1" // here
   }
 }
 
@@ -100,9 +156,11 @@ open class Family {
   }
   
   open func haveChild(_ child: Person) -> Bool {
+    return false // here
   }
   
   open func householdIncome() -> Int {
+    return 1 // here
   }
 }
 
